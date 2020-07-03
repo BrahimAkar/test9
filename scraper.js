5a7063c6787281a3d67a488abd2e5ef2ff0fb687
Brahim <brahim.akarouch@gmail.com>
2020-07-03 06:00:46 +0100

    fix2

--- scraper.js
+++ scraper.js
@@ -79,7 +79,7 @@ async function runScrapWithProxy(
       try {
         scrapObj = [];
         browser = await puppeteer.launch({
-          headless: false,
+          headless: true,
           args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
